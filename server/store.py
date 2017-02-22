0fbc5edeffa44ece2c0d8c5646b5705ecbe9d295
陈卓文 <chenzhuowen.simon@foxmail.com>
2017-02-22 20:39:53 +0800

    key value对象demo

---
+++ server/store.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class KVDB(dict):
+
+    def __init__(self, **kw):
+        super(KVDB, self).__init__(**kw)
+
+    def kget(self, key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError("has no attribute '%s'" % key)
+
+    def kvset(self, key, value):
+        self[key] = value
+
+if __name__ == '__main__':
+    a=KVDB()
+    #a['1']=2
+    a['2']=[1,2]
+    #print a
+    #a.kvset('2', [1, 2])
+    #print a.kget('2')
+    a.kvset('1', 2)
+    print a.kget('1')
+    print a
+
+
